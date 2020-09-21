cfcefcb9c991e929e0d76e1d2ff523cb336fded5
HRaval2 <Hardik.Raval2@T-Mobile.com>
2020-09-21 14:53:33 +0530

    Verify email after registration

--- config/fortify.php
+++ config/fortify.php
@@ -86,12 +86,12 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        // Features::emailVerification(),
+        Features::emailVerification(),
         Features::updateProfileInformation(),
         Features::updatePasswords(),
-        Features::twoFactorAuthentication([
+        /* Features::twoFactorAuthentication([
             'confirmPassword' => true,
-        ]),
+        ]), */
     ],
 
 ];
